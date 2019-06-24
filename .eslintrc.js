9c7851099b27050f9f54a7ebc1a4648889b596b2
Jonathan Andrade <1140117@unav.edu.mx>
2019-06-24 10:46:46 -0700

    Configure ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    parser: 'babel-eslint',
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
+        jest: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+    },
+};
